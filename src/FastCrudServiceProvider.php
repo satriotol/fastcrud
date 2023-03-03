583d37a2d0169566d57bf96a4453379a44113747
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-03 23:57:44 +0700

    fixing

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -27,25 +27,17 @@ class FastcrudServiceProvider extends ServiceProvider
 
         // Publish configuration file
         $this->publishes([
-            __DIR__ . '/resources/views' => resource_path('views/')
+            __DIR__ . '/resources/views' => resource_path('views/'),
+            __DIR__ . '/resources/stubs' => resource_path('stubs/'),
+            __DIR__ . '/database/migrations' => database_path('migrations/'),
+            __DIR__ . '/database/seeders' => database_path('seeders/'),
+            __DIR__ . '/public' => public_path('/'),
+            __DIR__ . '/routes/auth.php' => base_path('routes/auth.php'),
+            __DIR__ . '/routes/web.php' => base_path('routes/web.php'),
+            __DIR__ . '/routes/api.php' => base_path('routes/api.php'),
         ]);
-        $this->publishes([
-            __DIR__ . '/resources/stubs' => resource_path('stubs/')
-        ]);
-        $this->publishes([
-            __DIR__ . '/database/migrations' => database_path('migrations/')
-        ]);
-        $this->publishes([
-            __DIR__ . '/database/seeders' => database_path('seeders/')
-        ]);
-        $this->publishes([
-            __DIR__ . '/public' => public_path('/')
-        ]);
-        $this->publishes([
-            __DIR__ . '/routes/auth.php' => base_path('routes/auth.php')
-        ]);
-        $this->loadRoutesFrom(__DIR__ . '/routes/auth.php');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        // $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+        // $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         // $this->publishes([
         //     __DIR__ . '/routes' => base_path('routes/')
         // ]);
