28f7f4f6bf2936408052384ab8afd3dfec8781a0
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-03 23:40:42 +0700

    fixing updating routes

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -41,8 +41,10 @@ class FastcrudServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/public' => public_path('/')
         ]);
-        $this->publishes([
-            __DIR__ . '/routes' => base_path('routes/')
-        ]);
+        $this->loadRoutesFrom(__DIR__.'/packages/Satriotol/Fastcrud/src/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/packages/Satriotol/Fastcrud/src/routes/api.php');
+        // $this->publishes([
+        //     __DIR__ . '/routes' => base_path('routes/')
+        // ]);
     }
 }
