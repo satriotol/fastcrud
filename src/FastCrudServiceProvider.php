d5f6b30832781e3102b4170ba2af0cc01d8b0a38
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-08 23:14:27 +0700

    fixing

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -29,16 +29,26 @@ class FastCrudServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/resources/views' => resource_path('views/'),
-                __DIR__ . '/resources/stubs' => resource_path('stubs/'),
+
                 __DIR__ . '/routes/auth.php' => base_path('routes/auth.php'),
                 __DIR__ . '/routes/web.php' => base_path('routes/web.php'),
                 __DIR__ . '/routes/api.php' => base_path('routes/api.php'),
                 __DIR__ . '/lang' => base_path('lang/'),
-                __DIR__ . '/app' => app_path('/'),
+                __DIR__ . '/app/console' => app_path('/console'),
+                __DIR__ . '/app/Http' => app_path('/Http'),
+                __DIR__ . '/app/Models' => app_path('/Models'),
+                __DIR__ . '/app/helpers.php' => app_path('/'),
                 __DIR__ . '/config' => config_path('/'),
                 __DIR__ . '/database/migrations' => database_path('migrations/'),
                 __DIR__ . '/database/seeders' => database_path('seeders/'),
+                __DIR__ . '/resources/views' => resource_path('views/'),
             ], 'fastcrudStarter');
+            $this->publishes([
+                __DIR__ . '/app/Http/CrudController.php' => app_path('/Http'),
+                __DIR__ . '/app/Traits' => app_path('/Traits'),
+                __DIR__ . '/resources/stubs' => resource_path('stubs/'),
+                __DIR__ . '/resources/views/backend/' => resource_path('views/backend/crud'),
+            ], 'fastcrudContinue');
             $this->publishes([
                 __DIR__ . '/public' => public_path('/'),
             ], 'fastcrudPublic');
