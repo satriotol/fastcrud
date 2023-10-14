0d099afc5210c797e6fef1d4275b8a8b5996dfa4
Satrio Jati Wicaksono <satriotol69@gmail.com>
2023-10-14 16:29:45 +0700

    fixing bug

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -37,7 +37,7 @@ class FastCrudServiceProvider extends ServiceProvider
                 __DIR__ . '/app/Http' => app_path('/Http'),
                 __DIR__ . '/app/Models' => app_path('/Models'),
                 __DIR__ . '/app/Models/Providers/MenuServiceProvider.php' => app_path('/Models/Providers/MenuServiceProvider.php'),
-                __DIR__ . '/app/Helpers/Helpers.php' => app_path('app/Helpers/Helpers.php'),
+                __DIR__ . '/app/Helpers/Helpers.php' => app_path('/Helpers/Helpers.php'),
                 __DIR__ . '/config' => config_path('/'),
                 __DIR__ . '/database/migrations' => database_path('migrations/'),
                 __DIR__ . '/database/seeders' => database_path('seeders/'),
