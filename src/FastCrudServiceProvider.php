9beccb562e2c79a395963ceb68f796cf5fc52bfb
satriotol <satriotol69@gmail.com>
2024-05-26 21:29:04 +0700

    update api

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -29,6 +29,7 @@ class FastCrudServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/resources' => resource_path('/'),
+                __DIR__ . '/routes/api.php' => base_path('routes/api.php'),
                 __DIR__ . '/routes/auth.php' => base_path('routes/auth.php'),
                 __DIR__ . '/routes/web.php' => base_path('routes/web.php'),
                 __DIR__ . '/lang' => base_path('lang/'),
