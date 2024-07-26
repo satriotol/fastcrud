86f7d8f2132e6648927f73dc17bfa5c7c947856b
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2024-07-26 21:11:23 +0700

    remove unused

--- src/app/Providers/MenuServiceProvider.php
+++ src/app/Providers/MenuServiceProvider.php
@@ -19,12 +19,10 @@ class MenuServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
-    $verticalMenuData = json_decode($verticalMenuJson);
     $horizontalMenuJson = file_get_contents(base_path('resources/menu/horizontalMenu.json'));
     $horizontalMenuData = json_decode($horizontalMenuJson);
 
     // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData, $horizontalMenuData]);
+    \View::share('menuData', [$horizontalMenuData]);
   }
 }
