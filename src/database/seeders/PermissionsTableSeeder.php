f3d6578098bf7bc3017390a75cec0ef6245f0556
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2024-08-21 22:08:21 +0700

    fixing seeder

--- src/database/seeders/PermissionsTableSeeder.php
+++ src/database/seeders/PermissionsTableSeeder.php
@@ -14,237 +14,267 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('permissions')->delete();
-        
-        \DB::table('permissions')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('permissions')->insert(array(
+            0 =>
+            array(
                 'id' => 9,
                 'name' => 'permission-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:16:42',
                 'updated_at' => '2023-10-10 04:19:23',
             ),
-            1 => 
-            array (
+            1 =>
+            array(
                 'id' => 10,
                 'name' => 'permission-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:16:42',
                 'updated_at' => '2023-10-10 04:16:42',
             ),
-            2 => 
-            array (
+            2 =>
+            array(
                 'id' => 11,
                 'name' => 'permission-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:16:42',
                 'updated_at' => '2023-10-10 04:16:42',
             ),
-            3 => 
-            array (
+            3 =>
+            array(
                 'id' => 12,
                 'name' => 'permission-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:16:42',
                 'updated_at' => '2023-10-10 04:16:42',
             ),
-            4 => 
-            array (
+            4 =>
+            array(
                 'id' => 13,
                 'name' => 'permission-show',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:16:42',
                 'updated_at' => '2023-10-10 04:16:42',
             ),
-            5 => 
-            array (
+            5 =>
+            array(
                 'id' => 14,
                 'name' => 'role-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:34:25',
                 'updated_at' => '2023-10-10 04:34:25',
             ),
-            6 => 
-            array (
+            6 =>
+            array(
                 'id' => 15,
                 'name' => 'role-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:34:25',
                 'updated_at' => '2023-10-10 04:34:25',
             ),
-            7 => 
-            array (
+            7 =>
+            array(
                 'id' => 16,
                 'name' => 'role-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:34:25',
                 'updated_at' => '2023-10-10 04:34:25',
             ),
-            8 => 
-            array (
+            8 =>
+            array(
                 'id' => 17,
                 'name' => 'role-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:34:25',
                 'updated_at' => '2023-10-10 04:34:25',
             ),
-            9 => 
-            array (
+            9 =>
+            array(
                 'id' => 18,
                 'name' => 'role-show',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 04:34:25',
                 'updated_at' => '2023-10-10 04:34:25',
             ),
-            10 => 
-            array (
+            10 =>
+            array(
                 'id' => 20,
                 'name' => 'user-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 05:44:42',
                 'updated_at' => '2023-10-10 05:44:42',
             ),
-            11 => 
-            array (
+            11 =>
+            array(
                 'id' => 21,
                 'name' => 'user-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 05:44:42',
                 'updated_at' => '2023-10-10 05:44:42',
             ),
-            12 => 
-            array (
+            12 =>
+            array(
                 'id' => 22,
                 'name' => 'user-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 05:44:42',
                 'updated_at' => '2023-10-10 05:44:42',
             ),
-            13 => 
-            array (
+            13 =>
+            array(
                 'id' => 23,
                 'name' => 'user-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 05:44:42',
                 'updated_at' => '2023-10-10 05:44:42',
             ),
-            14 => 
-            array (
+            14 =>
+            array(
                 'id' => 24,
                 'name' => 'user-show',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-10 05:44:42',
                 'updated_at' => '2023-10-10 05:44:42',
             ),
-            15 => 
-            array (
+            15 =>
+            array(
                 'id' => 25,
                 'name' => 'crud-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-11 06:51:20',
                 'updated_at' => '2023-10-11 06:51:20',
             ),
-            16 => 
-            array (
+            16 =>
+            array(
                 'id' => 26,
                 'name' => 'crud-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-11 06:51:20',
                 'updated_at' => '2023-10-11 06:51:20',
             ),
-            17 => 
-            array (
+            17 =>
+            array(
                 'id' => 27,
                 'name' => 'crud-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-11 06:51:20',
                 'updated_at' => '2023-10-11 06:51:20',
             ),
-            18 => 
-            array (
+            18 =>
+            array(
                 'id' => 28,
                 'name' => 'crud-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-11 06:51:20',
                 'updated_at' => '2023-10-11 06:51:20',
             ),
-            19 => 
-            array (
+            19 =>
+            array(
                 'id' => 29,
                 'name' => 'crud-show',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-11 06:51:20',
                 'updated_at' => '2023-10-11 06:51:20',
             ),
-            20 => 
-            array (
+            20 =>
+            array(
                 'id' => 30,
                 'name' => 'media-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-14 04:07:40',
                 'updated_at' => '2023-10-14 04:07:40',
             ),
-            21 => 
-            array (
+            21 =>
+            array(
                 'id' => 31,
                 'name' => 'media-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-14 04:07:41',
                 'updated_at' => '2023-10-14 04:07:41',
             ),
-            22 => 
-            array (
+            22 =>
+            array(
                 'id' => 32,
                 'name' => 'media-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-14 04:07:41',
                 'updated_at' => '2023-10-14 04:07:41',
             ),
-            23 => 
-            array (
+            23 =>
+            array(
                 'id' => 33,
                 'name' => 'media-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-14 04:07:41',
                 'updated_at' => '2023-10-14 04:07:41',
             ),
-            24 => 
-            array (
+            24 =>
+            array(
                 'id' => 34,
                 'name' => 'menu-index',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-23 02:39:23',
                 'updated_at' => '2023-10-23 02:39:23',
             ),
-            25 => 
-            array (
+            25 =>
+            array(
                 'id' => 35,
                 'name' => 'menu-create',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-23 02:39:23',
                 'updated_at' => '2023-10-23 02:39:23',
             ),
-            26 => 
-            array (
+            26 =>
+            array(
                 'id' => 36,
                 'name' => 'menu-edit',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-23 02:39:23',
                 'updated_at' => '2023-10-23 02:39:23',
             ),
-            27 => 
-            array (
+            27 =>
+            array(
                 'id' => 37,
                 'name' => 'menu-delete',
                 'guard_name' => 'web',
                 'created_at' => '2023-10-23 02:39:23',
                 'updated_at' => '2023-10-23 02:39:23',
             ),
+            28 =>
+            array(
+                'id' => 38,
+                'name' => 'api_key-delete',
+                'guard_name' => 'web',
+                'created_at' => '2023-10-23 02:39:23',
+                'updated_at' => '2023-10-23 02:39:23',
+            ),
+            29 =>
+            array(
+                'id' => 39,
+                'name' => 'api_key-index',
+                'guard_name' => 'web',
+                'created_at' => '2023-10-23 02:39:23',
+                'updated_at' => '2023-10-23 02:39:23',
+            ),
+            30 =>
+            array(
+                'id' => 40,
+                'name' => 'api_key-create',
+                'guard_name' => 'web',
+                'created_at' => '2023-10-23 02:39:23',
+                'updated_at' => '2023-10-23 02:39:23',
+            ),
+            31 =>
+            array(
+                'id' => 41,
+                'name' => 'api_key-edit',
+                'guard_name' => 'web',
+                'created_at' => '2023-10-23 02:39:23',
+                'updated_at' => '2023-10-23 02:39:23',
+            ),
         ));
-        
-        
     }
 }
--- src/database/seeders/RoleHasPermissionsTableSeeder.php
+++ src/database/seeders/RoleHasPermissionsTableSeeder.php
@@ -14,178 +14,196 @@ class RoleHasPermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('role_has_permissions')->delete();
-        
-        \DB::table('role_has_permissions')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('role_has_permissions')->insert(array(
+            0 =>
+            array(
                 'permission_id' => 9,
                 'role_id' => 2,
             ),
-            1 => 
-            array (
+            1 =>
+            array(
                 'permission_id' => 10,
                 'role_id' => 2,
             ),
-            2 => 
-            array (
+            2 =>
+            array(
                 'permission_id' => 11,
                 'role_id' => 2,
             ),
-            3 => 
-            array (
+            3 =>
+            array(
                 'permission_id' => 12,
                 'role_id' => 2,
             ),
-            4 => 
-            array (
+            4 =>
+            array(
                 'permission_id' => 13,
                 'role_id' => 2,
             ),
-            5 => 
-            array (
+            5 =>
+            array(
                 'permission_id' => 14,
                 'role_id' => 2,
             ),
-            6 => 
-            array (
+            6 =>
+            array(
                 'permission_id' => 15,
                 'role_id' => 2,
             ),
-            7 => 
-            array (
+            7 =>
+            array(
                 'permission_id' => 16,
                 'role_id' => 2,
             ),
-            8 => 
-            array (
+            8 =>
+            array(
                 'permission_id' => 17,
                 'role_id' => 2,
             ),
-            9 => 
-            array (
+            9 =>
+            array(
                 'permission_id' => 18,
                 'role_id' => 2,
             ),
-            10 => 
-            array (
+            10 =>
+            array(
                 'permission_id' => 20,
                 'role_id' => 2,
             ),
-            11 => 
-            array (
+            11 =>
+            array(
                 'permission_id' => 20,
                 'role_id' => 4,
             ),
-            12 => 
-            array (
+            12 =>
+            array(
                 'permission_id' => 21,
                 'role_id' => 2,
             ),
-            13 => 
-            array (
+            13 =>
+            array(
                 'permission_id' => 21,
                 'role_id' => 4,
             ),
-            14 => 
-            array (
+            14 =>
+            array(
                 'permission_id' => 22,
                 'role_id' => 2,
             ),
-            15 => 
-            array (
+            15 =>
+            array(
                 'permission_id' => 22,
                 'role_id' => 4,
             ),
-            16 => 
-            array (
+            16 =>
+            array(
                 'permission_id' => 23,
                 'role_id' => 2,
             ),
-            17 => 
-            array (
+            17 =>
+            array(
                 'permission_id' => 23,
                 'role_id' => 4,
             ),
-            18 => 
-            array (
+            18 =>
+            array(
                 'permission_id' => 24,
                 'role_id' => 2,
             ),
-            19 => 
-            array (
+            19 =>
+            array(
                 'permission_id' => 24,
                 'role_id' => 4,
             ),
-            20 => 
-            array (
+            20 =>
+            array(
                 'permission_id' => 25,
                 'role_id' => 2,
             ),
-            21 => 
-            array (
+            21 =>
+            array(
                 'permission_id' => 26,
                 'role_id' => 2,
             ),
-            22 => 
-            array (
+            22 =>
+            array(
                 'permission_id' => 27,
                 'role_id' => 2,
             ),
-            23 => 
-            array (
+            23 =>
+            array(
                 'permission_id' => 28,
                 'role_id' => 2,
             ),
-            24 => 
-            array (
+            24 =>
+            array(
                 'permission_id' => 29,
                 'role_id' => 2,
             ),
-            25 => 
-            array (
+            25 =>
+            array(
                 'permission_id' => 30,
                 'role_id' => 2,
             ),
-            26 => 
-            array (
+            26 =>
+            array(
                 'permission_id' => 31,
                 'role_id' => 2,
             ),
-            27 => 
-            array (
+            27 =>
+            array(
                 'permission_id' => 32,
                 'role_id' => 2,
             ),
-            28 => 
-            array (
+            28 =>
+            array(
                 'permission_id' => 33,
                 'role_id' => 2,
             ),
-            29 => 
-            array (
+            29 =>
+            array(
                 'permission_id' => 34,
                 'role_id' => 2,
             ),
-            30 => 
-            array (
+            30 =>
+            array(
                 'permission_id' => 35,
                 'role_id' => 2,
             ),
-            31 => 
-            array (
+            31 =>
+            array(
                 'permission_id' => 36,
                 'role_id' => 2,
             ),
-            32 => 
-            array (
+            32 =>
+            array(
                 'permission_id' => 37,
                 'role_id' => 2,
             ),
+            33 =>
+            array(
+                'permission_id' => 38,
+                'role_id' => 2,
+            ),
+            34 =>
+            array(
+                'permission_id' => 39,
+                'role_id' => 2,
+            ),
+            35 =>
+            array(
+                'permission_id' => 40,
+                'role_id' => 2,
+            ),
+            36 =>
+            array(
+                'permission_id' => 41,
+                'role_id' => 2,
+            ),
         ));
-        
-        
     }
 }
