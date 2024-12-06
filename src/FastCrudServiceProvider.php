ded4c42cadefca8957b79d6a9bee4613ea735524
Satrio Jati Wicaksono <satriotol69@gmail.com>
2024-12-07 01:15:01 +0700

    fixing update

--- src/FastCrudServiceProvider.php
+++ src/FastCrudServiceProvider.php
@@ -24,10 +24,6 @@ class FastCrudServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/fastcrud_web.php');
-        if (!file_exists(__DIR__ . '/fastcrud_web.php')) {
-            dd('Route file not found');
-        }
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/resources' => resource_path('/'),
@@ -35,6 +31,7 @@ class FastCrudServiceProvider extends ServiceProvider
                 __DIR__ . '/routes/auth.php' => base_path('routes/auth.php'),
                 __DIR__ . '/routes/web.php' => base_path('routes/web.php'),
                 __DIR__ . '/routes/fastcrud_web_generator.php' => base_path('routes/fastcrud_web_generator.php'),
+                __DIR__ . '/routes/fastcrud_web.php' => base_path('routes/fastcrud_web.php'),
                 __DIR__ . '/lang' => base_path('lang/'),
                 __DIR__ . '/app' => app_path('/'),
                 __DIR__ . '/bootstrap' => base_path('bootstrap/'),
