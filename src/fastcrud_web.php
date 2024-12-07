ca57783780acd9c639d7f60f1df43f754da02626
Satrio Jati Wicaksono <satriotol69@gmail.com>
2024-12-07 12:41:50 +0700

    fixing web

--- src/fastcrud_web.php
+++ src/fastcrud_web.php
@@ -13,25 +13,26 @@ use Satriotol\Fastcrud\Controllers\AppSpecsController;
 use Satriotol\Fastcrud\Controllers\AuditController;
 
 Route::prefix('admin')->group(function () {
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/change-password', [PasswordController::class, 'showChangePasswordForm'])->name('password.change.form');
-        Route::post('/change-password', [PasswordController::class, 'changePassword'])->name('password.change');
-    });
-    Route::middleware(['auth', 'force.password.change'])->group(function () {
-        Route::get('app-specs', [AppSpecsController::class, 'index'])->name('app-specs.index');
-        Route::get('audit', [AuditController::class, 'index'])->name('audit.index');
-        Route::get('profile', [UserController::class, 'profile'])->name('user.profile');      
-        Route::put('/updateProfile', [UserController::class, 'updateProfile'])->name('user.updateProfile');
-        Route::resource('crud', CrudController::class);
-        Route::resource('api_key', ApiKeyController::class);
-        Route::resource('config', ConfigController::class);
-        Route::post('/user/reset-password/{uuid}', [PasswordController::class, 'resetPassword'])->name('user.resetPassword');
-        Route::post('/users/reset-passwords', [PasswordController::class, 'resetPasswords'])->name('users.resetPasswords');
-        Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-        Route::resource('menu', MenuController::class);
-        Route::resource('media', MediaController::class);      
+    Route::middleware(['web'])->group(function () {
+        Route::get('getfile', [MinioController::class, 'getfile'])->name('minio.getfile');
+        Route::get('getConfig/{uuid}', [ConfigController::class, 'getConfig'])->name('config.getConfig');
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/change-password', [PasswordController::class, 'showChangePasswordForm'])->name('password.change.form');
+            Route::post('/change-password', [PasswordController::class, 'changePassword'])->name('password.change');
+        });
+        Route::middleware(['auth', 'force.password.change'])->group(function () {
+            Route::get('app-specs', [AppSpecsController::class, 'index'])->name('app-specs.index');
+            Route::get('audit', [AuditController::class, 'index'])->name('audit.index');
+            Route::get('profile', [UserController::class, 'profile'])->name('user.profile');
+            Route::put('/updateProfile', [UserController::class, 'updateProfile'])->name('user.updateProfile');
+            Route::resource('crud', CrudController::class);
+            Route::resource('api_key', ApiKeyController::class);
+            Route::resource('config', ConfigController::class);
+            Route::post('/user/reset-password/{uuid}', [PasswordController::class, 'resetPassword'])->name('user.resetPassword');
+            Route::post('/users/reset-passwords', [PasswordController::class, 'resetPasswords'])->name('users.resetPasswords');
+            Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+            Route::resource('menu', MenuController::class);
+            Route::resource('media', MediaController::class);
+        });
     });
 });
-
-Route::get('getfile', [MinioController::class, 'getfile'])->name('minio.getfile');
-Route::get('getConfig/{uuid}', [ConfigController::class, 'getConfig'])->name('config.getConfig');
