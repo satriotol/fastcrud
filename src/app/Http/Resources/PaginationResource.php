4ba36f7933f7b9e1ab6fc987548ea1a72ae0abd0
Satrio Jati Wicaksono <satriotol69@gmail.com>
2025-02-26 08:35:54 +0700

    build(deps): update composer.json

---
+++ src/app/Http/Resources/PaginationResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaginationResource extends JsonResource
+{
+    public function __construct($resource)
+    {
+        parent::__construct($resource);
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request)
+    {
+        return [
+            'current_page' => $this->currentPage(),
+            'from' => $this->firstItem(),
+            'last_page' => $this->lastPage(),
+            'path' => $this->path(),
+            'per_page' => $this->perPage(),
+            'to' => $this->lastItem(),
+            'total' => $this->total()
+        ];
+    }
+}
