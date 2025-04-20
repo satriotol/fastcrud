fd574ab5bcc40db5e153ec98c0e0be0a0da55652
Satrio Jati Wicaksono <satriotol69@gmail.com>
2025-04-20 09:36:32 +0700

    update Google2FAMiddleware.php

--- src/Middleware/Google2FAMiddleware.php
+++ src/Middleware/Google2FAMiddleware.php
@@ -5,6 +5,7 @@ namespace Satriotol\Fastcrud\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class Google2FAMiddleware
 {
@@ -15,6 +16,15 @@ class Google2FAMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        // Dapatkan daftar role yang dikecualikan dari .env (dipisahkan dengan koma)
+        $excludedRoles = explode(',', env('GOOGLE2FA_EXCEPT_ROLES', ''));
+
+        // Cek jika user login dan memiliki salah satu role yang dikecualikan
+        $user = Auth::user();
+        if ($user && $user->hasAnyRole($excludedRoles)) {
+            return $next($request); // Lewati pengecekan 2FA
+        }
+
         if (!session()->has('2fa_verified') && env('APP_DEBUG') == false) {
             return redirect()->route('2fa.setup');
         }
